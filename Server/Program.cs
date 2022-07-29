5e55624825be2d6f2a6f2490d704e54fa902ad59
Plorence <ploseok@gmail.com>
2022-07-29 10:59:02 +0900

    최상위 문 스타일로 변경

--- Server/Program.cs
+++ Server/Program.cs
@@ -3,30 +3,22 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 
 using System.Globalization;
+using Server;
 
-namespace Server
-{
-    public class Program
-    {
-        public static void Main(string[] args)
+CultureInfo.DefaultThreadCurrentCulture = CultureInfo.GetCultureInfoByIetfLanguageTag("ko-kr");
+
+CreateHostBuilder(args).Build().Run();
+
+static IHostBuilder CreateHostBuilder(string[] args) =>
+    Host.CreateDefaultBuilder(args)
+        .ConfigureAppConfiguration((hostingContext, config) =>
         {
-            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.GetCultureInfoByIetfLanguageTag("ko-kr");
-
-            CreateHostBuilder(args).Build().Run();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureAppConfiguration((hostingContext, config) =>
-                {
-                    config.AddEnvironmentVariables(prefix: "APP_");
-                })
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
+            config.AddEnvironmentVariables(prefix: "APP_");
+        })
+        .ConfigureWebHostDefaults(webBuilder =>
+        {
+            webBuilder.UseStartup<Startup>();
 #if !DEBUG
                     webBuilder.UseSentry();
 #endif
-                });
-    }
-}
+        });
